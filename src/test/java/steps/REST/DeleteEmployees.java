42f4632266f5e5ec7c025631080d51d6d89b368b
DimitarKKirov <dimitar.kirov@estafet.com>
2020-09-25 09:44:48 +0300

    rest

---
+++ src/test/java/steps/REST/DeleteEmployees.java
@@ -0,0 +1,19 @@
+package steps.REST;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DeleteEmployees {
+
+    @When("user gives {string} of the emplyee")
+    public void user_gives_of_the_emplyee(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the emplyees who are with this IDs are deleted")
+    public void the_emplyees_who_are_with_this_i_ds_are_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
